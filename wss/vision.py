5ad8e547fc132f5750fd9d0db549b34154a899ef
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-05-01 11:45:48 -0700

    Log visible squares in Vision

--- wss/vision.py
+++ wss/vision.py
@@ -1,16 +1,34 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from abc import ABC, abstractmethod
-
 if TYPE_CHECKING:
     from player import Player
 
+import logging
 
-class Vision(ABC):
-    @abstractmethod
+logger = logging.getLogger(__name__)
+
+
+class Vision():
+    fov = [(0, 0)]
+
     def get_visible_positions(self, player: Player) -> list[tuple[int, int]]:
-        pass
+        y_limit = len(player.map.terrain)
+        x_limit = len(player.map.terrain[0])
+        visible_positions = [(player.y, player.x)]
+
+        for dy, dx in self.fov:
+            if (
+                player.y + dy >= 0
+                and player.y + dy < y_limit
+                and player.x + dx >= 0
+                and player.x + dx < x_limit
+            ):
+                visible_positions.append((player.y + dy, player.x + dx))
+
+        logger.debug(f"Player {player.icon} at ({player.y}, {player.x}), sees {visible_positions}")
+
+        return visible_positions
 
     def closestFood(self):
         pass
@@ -31,18 +49,14 @@ class Vision(ABC):
 class FocusedVision(Vision):
     fov = [(-1, 1), (0, 1), (1, 1)]
 
-    def get_visible_positions(self, player: Player) -> list[tuple[int, int]]:
-        y_limit = len(player.map.terrain)
-        x_limit = len(player.map.terrain[0])
-        visible_positions = [(player.y, player.x)]
 
-        for dy, dx in self.fov:
-            if (
-                player.y + dy >= 0
-                and player.y + dy < y_limit
-                and player.x + dx >= 0
-                and player.x + dx < x_limit
-            ):
-                visible_positions.append((player.y + dy, player.x + dx))
+class CautiousVision(Vision):
+    fov = [(-1, 0), (0, 1), (1, 1)]
 
-        return visible_positions
+
+class KeenEyedVision(Vision):
+    fov = [(-1, 0), (-1, 1), (0, 1), (0, 2), (1, 0), (1, 1)]
+
+
+class FarSightVision(Vision):
+    fov = [(-2, 0), (-2, 1), (-1, 0), (-1, 1), (-1, 2), (0, 1), (0,2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1)]
